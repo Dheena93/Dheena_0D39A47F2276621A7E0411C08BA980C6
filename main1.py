3bb3a44c7e866f809e6fe39f73a6c669ae16c9a6
Dheena93 <144871472+Dheena93@users.noreply.github.com>
2023-09-15 14:19:53 +0530

    Add files via upload

--- main1.py
+++ main1.py
@@ -1,14 +1,14 @@
-def recur_factorial(n):  
-   if n == 1:  
-       return n  
-   else:  
-       return n*recur_factorial(n-1)  
-# take input from the user  
-num = int(input("Enter a number: "))  
-# check is the number is negative  
-if num < 0:  
-   print("Sorry, factorial does not exist for negative numbers")  
-elif num == 0:  
-   print("The factorial of 0 is 1")  
-else:  
-   print("The factorial of",num,"is",recur_factorial(num))
+class player:
+  def play(self):
+       print(" The player is playing cricket.")
+class Batsman(player):
+  def play(self):
+       print(" The batsman is batting.")
+class Bowler(player):
+  def play(self):
+       print(" The bowler is bowling.")
+batsman= Batsman()
+bowler= Bowler()
+
+batsman.play()
+bowler.play()
